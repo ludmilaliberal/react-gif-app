f130d16d419f1a27f49ba51a1791976348c8b67f
Ludmila <ludmilaliberal@gmail.com>
2022-08-30 12:01:05 -0300

    Arreglando error http

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category)=>{
-    const url = `http://api.giphy.com/v1/gifs/search?apikey=zyGPyKIt25pJDpArvWQ3gHXMTMK6DjPM&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?apikey=zyGPyKIt25pJDpArvWQ3gHXMTMK6DjPM&q=${category}&limit=10`;
     const respuesta = await fetch (url);
     const {data} = await respuesta.json();
 
